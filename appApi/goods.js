7e2c19cd62423c8233880b2c6ea8fc36d014a310
unknown <https://github.com/Danneil-525/my-notes.git>
2021-02-24 01:51:55 +0800

    feat: 插入商品大类接口完成

--- appApi/goods.js
+++ appApi/goods.js
@@ -3,6 +3,9 @@ let router = new Router()
 
 const mongoose = require('mongoose')
 const fs = require('fs')
+const {
+  count
+} = require('console')
 
 router.get('/insertAllGoodsInfo', async (ctx) => {
   fs.readFile('./data_json/goods.json', 'utf-8', (err, data) => {
@@ -15,9 +18,31 @@ router.get('/insertAllGoodsInfo', async (ctx) => {
       newGoods.save().then(() => {
         saveCount++
         console.log("插入成功" + saveCount);
-      }).catch(err => { console.log(err); })
+      }).catch(err => {
+        console.log(err);
+      })
     })
   })
   ctx.body = "开始导入数据"
 })
+
+router.get('/insertAllCategory', async (ctx) => {
+  fs.readFile('./data_json/category.json', async (err, data) => {
+    data = JSON.parse(data)
+    let saveCount = 0
+    const Category = mongoose.model('Category')
+    data.RECORDS.map((val) => {
+      console.log(val);
+      let newCategory = new Category(val)
+      newCategory.save().then(() => {
+        saveCount++;
+        console.log("成功" + saveCount);
+      }).catch((err) => {
+        console.log("失败", err);
+      })
+    })
+  })
+  ctx.body = "开始导入数据"
+})
+
 module.exports = router;
